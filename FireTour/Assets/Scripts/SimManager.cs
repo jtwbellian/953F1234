acf50f2d83faa3acdc588722bb38166de7aef199
BinariDan <dabellian@gmail.com>
2019-05-29 15:52:35 -0500

    stable build from solarmar meeting

---
+++ FireTour/Assets/Scripts/SimManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SimManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("load");
+    }
+
+    public string levelName;
+    AsyncOperation async;
+
+    IEnumerator load()
+    {
+        Debug.LogWarning("ASYNC LOAD STARTED - " +
+           "DO NOT EXIT PLAY MODE UNTIL SCENE LOADS... UNITY WILL CRASH");
+        async = SceneManager.LoadSceneAsync(levelName);
+        //async.allowSceneActivation = false;
+        yield return async;
+    }
+
+    public void ActivateScene()
+    {
+        async.allowSceneActivation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

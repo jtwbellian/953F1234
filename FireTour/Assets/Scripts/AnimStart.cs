dd543aecb8a69b218238c46f6f1a5fdadf9d6df8
jtwbellian <32314548+jtwbellian@users.noreply.github.com>
2019-06-03 20:41:58 -0500

    animated firefighters

---
+++ FireTour/Assets/Scripts/AnimStart.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimStart : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+    anim = GetComponent<Animator>();
+        anim.speed = 1f;
+    }
+}

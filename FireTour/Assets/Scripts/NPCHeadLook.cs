55e3fe305a7c7434a24e7ed3af3f0428a1841304
Daniel <dabellian@gmail.com>
2019-06-04 21:20:04 -0500

    look at player script in progress

---
+++ FireTour/Assets/Scripts/NPCHeadLook.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCHeadLook : MonoBehaviour
+{
+    public GameObject target;
+    private Animator myAnimator;
+    private bool lineOfSight;
+  
+    void Start()
+    {
+        SetInitialReferences(); 
+    }
+
+    void SetInitialReferences()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (lineOfSight == false)
+            lineOfSight = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (lineOfSight == true)
+            lineOfSight = false;
+    }
+
+    void OnAnimatorIK()
+    {
+        if (myAnimator.enabled)
+        {
+            if (lineOfSight == true && target!=null)
+            {
+                myAnimator.SetLookAtWeight(1, 0, 0.5f, 0, 0.7f);
+                myAnimator.SetLookAtPosition(target.transform.position);
+            }
+            else
+            {
+                myAnimator.SetLookAtWeight(0);
+            }
+        }
+    }
+}
---
+++ FireTour/Assets/Scripts/TriggerEvent.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class TriggerEvent : MonoBehaviour
+{
+    private GameObject target;
+    public string targetName;
+
+    void OnTriggerStay(Collider Other)
+    {
+        if (Other.gameObject.tag == "Player")
+        {
+            if (transform.Find(targetName).gameObject != null)
+            {
+                target = transform.Find(targetName).gameObject;
+
+                if (target.activeSelf == false)
+                {
+                    target.gameObject.SetActive(true);
+                }
+            }
+
+        }
+    }
+    private void OnTriggerExit(Collider Other)
+    {
+        if (Other.gameObject.tag == "Player")
+        {
+            if (transform.Find(targetName).gameObject != null)
+            {
+                target = transform.Find(targetName).gameObject;
+
+                if (target.activeSelf == true)
+                {
+                    target.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}

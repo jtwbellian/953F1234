38936dbc8317fa081342741f046ca8ab71e68b95
Daniel Bellian <dabellian@gmail.com>
2019-09-05 17:04:56 -0500

    navmesh and early Firefighter AI

---
+++ FireTour/Assets/Scripts/FireFighterController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class FireFighterController : MonoBehaviour
+{
+    public GameObject target;
+    public NavMeshAgent agent;
+    public Animator animator;
+
+    private float maxSpeed;
+
+    // Update is called once per frame
+    void Start()
+    {
+     maxSpeed = agent.speed;
+     agent.SetDestination(target.transform.position);
+    }
+
+    void Update()
+    {
+    animator.SetFloat("Speed", agent.velocity.magnitude / maxSpeed);
+    }
+}
---
+++ FireTour/Assets/Scripts/SetHead.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetHead : MonoBehaviour
+{
+
+    public GameObject[] allHeads;
+
+    public int headIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      for (int i = 0; i < allHeads.Length; i++)
+        {
+            if (i == headIndex)
+            {
+                allHeads[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                allHeads[i].gameObject.SetActive(false);
+            }
+        }
+    }
+
+}

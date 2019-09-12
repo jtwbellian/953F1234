f57645ea11ee3304848f4d523260cbba350a284f
Daniel Bellian <dabellian@gmail.com>
2019-09-12 14:52:23 -0500

    2D trees added

--- FireTour/Assets/Scripts/CatalogVerticies.cs
+++ FireTour/Assets/Scripts/CatalogVerticies.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CatalogVerticies : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject targetMesh;
 
-    // Update is called once per frame
-    void Update()
+[ContextMenu("Bake Vertex Catalog")]
+    void Catalog()
     {
-        
+    MeshFilter mf = targetMesh.GetComponent<MeshFilter>();
+    Matrix4x4 localToWorld = transform.localToWorldMatrix;
+ 
+    for(int i = 0; i<mf.sharedMesh.vertices.Length; ++i)
+        {
+        Vector3 world_v = localToWorld.MultiplyPoint3x4(mf.sharedMesh.vertices[i]);
+        Debug.Log("Index: " + i.ToString() + "  Worldspace Location:" + world_v.ToString());
+        }
+    
     }
 }

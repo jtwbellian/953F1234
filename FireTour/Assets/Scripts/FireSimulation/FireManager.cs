11e1e44a02fa1c2e9c8a60a53fea746f3a825e86
Daniel Bellian <dabellian@gmail.com>
2019-09-14 03:47:56 -0500

    vertex magic and baking probe data

--- FireTour/Assets/Scripts/FireSimulation/FireManager.cs
+++ FireTour/Assets/Scripts/FireSimulation/FireManager.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class FireManager : MonoBehaviour
 {
+    private Mesh mesh;
+    public MeshFilter targetMesh;
+    public GameObject targetNodeGroup;
     public List<FireProbe> probes;
     public WindHorn windHorn;
     private FXManager fx;
@@ -11,6 +15,11 @@ public class FireManager : MonoBehaviour
 
     [SerializeField, Range(0.01f, 50f)]
     private float timeToDouble = 30f;
+
+
+    ////////////////////////////////////////////
+    public GameObject testPoint = null;
+    ///////////////////////////////////////////
 
     #region singleton
 
@@ -72,4 +81,71 @@ public class FireManager : MonoBehaviour
 
         yield return null;
     }
+
+    public void SetVertexColor(int index, Color color)
+    {
+        mesh.colors[index] = color;
+    }
+
+    [ContextMenu("Collect All Probes")]
+
+    void CollectProbes()
+    {
+        probes = targetNodeGroup.GetComponentsInChildren<FireProbe>().ToList();
+        foreach (var probe in probes)
+        {
+            probe.Start();
+        }
+    }
+
+    [ContextMenu("Bake Vertex Catalog")]
+    void Catalog()
+    {
+        mesh = targetMesh.sharedMesh;
+        //Matrix4x4 localToWorld = transform.localToWorldMatrix;
+
+        //Create empty array of proper size to manage mesh Vertex count
+
+        Vector3[] vertPos = new Vector3[mesh.vertices.Length];
+
+        //Find the worldspace equivalent of every vertex location. 
+        //Then, add that location to our vertLocs array. 
+
+        for(int i = 0; i < mesh.vertices.Length; ++i)
+        {
+            Vector3 world_v = mesh.vertices[i]; //localToWorld.MultiplyPoint3x4(mesh.vertices[i]);
+            vertPos[i] = world_v + new Vector3(-2.69f, 0.0568691f, 20.52f); //+ targetMesh.transform.position;
+            Debug.Log("Index: " + i.ToString() + "  Worldspace Location:" + vertPos[i].ToString());
+
+            /*if (i < 2000)
+            {            
+                var obj = Instantiate(testPoint);
+                obj.transform.position = vertPos[i];
+                //obj.transform.SetParent(testPoint.transform);
+            }*/
+        }
+
+            //Delegate vertex points to each Node
+        foreach (var probe in probes)
+        {
+            Debug.Log("Probe " + probe.ToString());
+            int i = 0;
+            var currentShellGroup = new List<int>();
+            Debug.Log("This is Vertpos Length: " + vertPos.Length.ToString());
+
+            for(int k = 0; k < vertPos.Length; k++)
+                {Debug.Log("Checking vertex " + k.ToString());
+
+                    if (Vector3.Distance(probe.transform.position, vertPos[k]) < probe.trigger.radius)
+                    {
+                        currentShellGroup.Add(k);
+                        Debug.Log("Probe " + probe.ToString() + " was given Vertex "  + k.ToString());
+                    }
+                }
+
+            probe.VertexGroup[i] = currentShellGroup;
+        }
+        
+        }
+ 
 }
--- FireTour/Assets/Scripts/FireSimulation/FireProbe.cs
+++ FireTour/Assets/Scripts/FireSimulation/FireProbe.cs
@@ -9,13 +9,20 @@ public class FireProbe : MonoBehaviour
 
     private int num = 1;
     private bool lit = false;
-    private float growRate = 2f;
-    private float startRadius = 0.5f;
+    private const float growRate = 2f;
+    private const float startRadius = 0.5f;
     private FireManager fm;
     private FXManager fx;
-    private SphereCollider trigger;
+    public SphereCollider trigger;
     private Rigidbody rb; 
     private float updateTime = 5f;
+
+    //Calculuate number of times a probe can expand before reaching MAX_RADIUS
+    private static int shellCount = Mathf.RoundToInt(Mathf.Log(MAX_RADIUS / startRadius) / Mathf.Log(growRate)); 
+
+    public int shellIndex = 0;
+
+    public List<int>[] VertexGroup = new List<int>[(shellCount)];
 
     public FXManager.PType fireType = FXManager.PType.Flame1;
     public FXManager.PType smokeType = FXManager.PType.Smoke1;
@@ -34,7 +41,7 @@ public class FireProbe : MonoBehaviour
     }
 
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
         fm = FireManager.GetInstance();
         fx = FXManager.GetInstance();
@@ -71,7 +78,26 @@ public class FireProbe : MonoBehaviour
     public void Refresh()
     {
         if (trigger.radius < MAX_RADIUS)
+        {
+            List<int> shellList = VertexGroup[shellIndex];
+
+            if (shellList != null)
+            {
+                for (int i = 0; i < shellList.Count; ++i)
+                {
+                    //shellList contains several integers that each reference a particular vertex in the mesh.
+                    //This for loop is cycling through each of these integers and matching them up with their 
+                    //equivalent counterparts in the array of mesh vertex colors.  
+                    fm.SetVertexColor(shellList[i], Color.red);
+                }
+            }
+            //grow the probe
             trigger.radius = trigger.radius * growRate;
+            //prep the next shell for the next Refresh.
+            if (shellIndex<shellCount)
+                shellIndex+=1;
+        }
+
     }
 
     IEnumerator Burn()

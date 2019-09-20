6e30af4ef96cc081d921a080da69c25fb9608e71
Daniel Bellian <dabellian@gmail.com>
2019-09-19 19:22:24 -0500

    minor adjustments to navmesh

--- FireTour/Assets/Scripts/TriggerEvent.cs
+++ FireTour/Assets/Scripts/TriggerEvent.cs
@@ -3,27 +3,39 @@
 public class TriggerEvent : MonoBehaviour
 {
     public GameObject target;
+    public bool invertActivation;
     public bool persistAfterDeparture;
 
     void OnTriggerEnter(Collider Other)
     {
-        if (Other.gameObject.transform.root.tag == "Player")
+        if (target !=null && Other.gameObject.transform.root.tag == "Player")
         {
-            if (target != null && target.activeSelf==false)
-            {
-                target.gameObject.SetActive(true);
-            }
-
+                if (invertActivation)
+                {
+                    if (target.activeSelf)
+                        target.gameObject.SetActive(false);
+                }  
+                else
+                {
+                    if (!target.activeSelf)
+                        target.gameObject.SetActive(true);
+                }   
         }
     }
     private void OnTriggerExit(Collider Other)
-    {
-        if (Other.gameObject.transform.root.tag == "Player")
         {
-            if (target != null && target.activeSelf==true && persistAfterDeparture==false)
-            { 
-                    target.gameObject.SetActive(false);
-            }
+        if (target !=null && Other.gameObject.transform.root.tag == "Player")
+        {
+                if (invertActivation)
+                {
+                    if (!target.activeSelf)
+                        target.gameObject.SetActive(true);
+                }  
+                else
+                {
+                    if (target.activeSelf)
+                        target.gameObject.SetActive(false);
+                }   
         }
     }
 }
